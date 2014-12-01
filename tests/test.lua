10c134fdbe1cddbd4d780be943d9f901a0d149ed
Shuxin Yang <shuxinyang2006@gmail.com>
2014-11-30 16:46:30 -0800

    add testing for lua wrapper

---
+++ tests/test.lua
@@ -0,0 +1,69 @@
+package.cpath = package.cpath..";../?.so"
+package.path = package.cpath..";../?.lua"
+
+local ljson_decoder = require 'json_decoder'
+local f, err = io.open("../t2.json", "r")
+
+local function cmp_lua_var(obj1, obj2)
+    if type(obj1) ~= type(obj2) then
+        return
+    end
+
+    if type(obj1) == "string" or
+       type(obj1) == "number" or
+       type(obj1) == "ni" or
+       type(obj1) == "boolean" then
+        return obj1 == obj2 and true or nil
+    end
+
+    if (type(obj1) ~= "table") then
+        print("unknown type", type(obj1));
+        return
+    end
+
+    -- compare table
+    for k, v in pairs(obj1) do
+        if not cmp_lua_var(v, obj2[k]) then
+            -- print(v," vs ", obj2[k])
+            return
+        end
+    end
+
+    for k, v in pairs(obj2) do
+        if not cmp_lua_var(v, obj1[k]) then
+            -- print(v," vs ", obj1[k])
+            return
+        end
+    end
+
+    return true
+end
+
+local test_fail_num = 0;
+local test_total = 0;
+
+local function ljson_test(test_id, parser, input, expect)
+    test_total = test_total + 1
+    io.write(string.format("Testing %s ...", test_id))
+    local result = ljson_decoder.parse(parser, input)
+    if cmp_lua_var(result, expect) then
+        print("succ!")
+    else
+        test_fail_num = test_fail_num + 1
+        print("failed!")
+        --ljson_decoder.debug(result)
+    end
+end
+
+local json_parser = ljson_decoder.create()
+
+local input = [=[[1, 2, 3, {"key1":"value1", "key2":"value2"}, "lol"]]=]
+local output = {1, 2, 3, {["key1"] = "value1", ["key2"] = "value2" }, "lol"}
+
+ljson_test("test1", json_parser, input, output);
+
+io.write(string.format(
+        "\n============================\nTotal test count %d, fail %d\n",
+        test_total, test_fail_num))
+
+--TODO: How to let luajit exit with non-zero if not all testing case pass.
